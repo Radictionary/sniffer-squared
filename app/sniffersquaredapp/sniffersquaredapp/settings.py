3bd6bb8f5051a3b17a0c5999d8489f9f0f7fe862
siIverfish <ivan.g.zimmerman@gmail.com>
2024-03-23 14:16:08 -0700

    django basics

--- app/sniffersquaredapp/sniffersquaredapp/settings.py
+++ app/sniffersquaredapp/sniffersquaredapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # mine
+    'sniffersquaredui',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'sniffersquaredapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/sniffersquaredapp/sniffersquaredui/views.py
+++ app/sniffersquaredapp/sniffersquaredui/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def index(request):
+    return render(
+        request,
+        "index.html",
+        context=dict(title="Your Console")
+    )
+
+@login_required
+def history(request):
+    return render(
+        request,
+        "history.html",
+        context=dict(title="Your History")
+    )
+
+routes = [
+    ('', index), 
+    ('history/', history)
+]
