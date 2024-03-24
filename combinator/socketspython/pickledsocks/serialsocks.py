dcb0568c56837b56e9d62c0064656e8252a686af
siIverfish <ivan.g.zimmerman@gmail.com>
2024-03-23 21:42:41 -0700

    readme update

---
+++ combinator/socketspython/pickledsocks/serialsocks.py
@@ -0,0 +1,37 @@
+"""
+    Facilitate connections processes.
+
+    Python programs communicate amongst themselves with `pickle` or `json`.
+    They must communicate with non-python programs with `json`, because
+    of course non-python processes cannot use `pickle`.
+"""
+
+import zmq.asyncio
+from typing import Any, Callable
+
+# create a ZeroMQ context. 
+context = zmq.asyncio.Context.instance()
+
+# ------------ important functions ------------
+
+async def make_server(serializer, f: Callable, port: int):
+    """
+        f: a handler function which will receive the data from the port.
+        port: the port.
+    """
+    socket = context.socket(zmq.REP)
+    socket.bind(f"tcp://*:{port}")
+    while True:
+        data = serializer.loads(await socket.recv())
+        result = serializer.dumps(f(data))
+        await socket.send(result)
+
+async def send(serializer, data: Any, port: int):
+    """
+        send data to server at port. data can be anything.
+    """
+    socket = context.socket(zmq.REQ)
+    socket.connect(f"tcp://localhost:{port}")
+    await socket.send(serializer.dumps(data))
+    reply = serializer.loads(await socket.recv())
+    return reply
