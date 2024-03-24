a0890027a92e1423ac1c8a4477abdf7495562f2e
siIverfish <ivan.g.zimmerman@gmail.com>
2024-03-24 12:43:04 -0700

    ps 0.1.8

--- combinator/socketspython/pickledsocks/picklesocks.py
+++ combinator/socketspython/pickledsocks/picklesocks.py
@@ -4,5 +4,5 @@ from functools import partial
 
 import pickle
 
-make_server = partial(serialsocks.make_server, pickle)
-send = partial(serialsocks.send, pickle)
+make_server = partial(serialsocks.make_server, pickle.dumps, pickle.loads)
+send = partial(serialsocks.send, pickle.dumps, pickle.loads)
--- combinator/socketspython/pickledsocks/serialsocks.py
+++ combinator/socketspython/pickledsocks/serialsocks.py
@@ -14,7 +14,7 @@ context = zmq.asyncio.Context.instance()
 
 # ------------ important functions ------------
 
-async def make_server(serializer, f: Callable, port: int):
+async def make_server(dumper, loader, f: Callable, port: int):
     """
         f: a handler function which will receive the data from the port.
         port: the port.
@@ -22,16 +22,16 @@ async def make_server(serializer, f: Callable, port: int):
     socket = context.socket(zmq.REP)
     socket.bind(f"tcp://*:{port}")
     while True:
-        data = serializer.loads(await socket.recv())
-        result = serializer.dumps(f(data))
+        data = loader(await socket.recv())
+        result = dumper(f(data))
         await socket.send(result)
 
-async def send(serializer, data: Any, port: int):
+async def send(dumper, loader, data: Any, port: int):
     """
         send data to server at port. data can be anything.
     """
     socket = context.socket(zmq.REQ)
     socket.connect(f"tcp://localhost:{port}")
-    await socket.send(serializer.dumps(data))
-    reply = serializer.loads(await socket.recv())
+    await socket.send(dumper(data))
+    reply = loader(await socket.recv())
     return reply
