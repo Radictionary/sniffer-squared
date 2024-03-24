2f22dbcb011d45db84fbbee40cfb7ed0c7ce96c9
Kelly <kellygao725@gmail.com>
2024-03-24 10:18:32 -0700

    edit

--- traffic_analysis/analysis.py
+++ traffic_analysis/analysis.py
@@ -1,11 +1,7 @@
 from scapy.all import *
 import requests
 from bs4 import BeautifulSoup
-
-def process_pcap(pcap_file):
-    packets = rdpcap(pcap_file)  # Read PCAP file into memory
-    for packet in packets:
-        print(process_packet(packet))  # Processing each packet and printing the information
+import os
 
 
 def get_protocol_name(protocol_number):
@@ -16,9 +12,18 @@ def get_protocol_name(protocol_number):
     return None
 
 
-def process_packet(packet):
+def process_pcap(packet):
     # Initialize a list to store extracted information
     extracted_info = []
+
+    d = {}
+    with open("traffic_analysis\service-names-port-numbers.csv") as f:
+        for line in f:
+            try:
+                key, val = line.strip().split(',')
+                d[val] = key
+            except ValueError:
+                print(line.split(','))
 
     # Open the PCAP file and process each packet
     '''id.orig_p: Source Port
@@ -34,84 +39,149 @@ def process_packet(packet):
     orig_ip_bytes: Original IP Bytes
     resp_pkts: Response Packets Count
     resp_ip_bytes: Response IP Bytes'''
+    # rdp = rdpccap(pcap_file)
+    # l = len(rdp)
     for packet in rdpcap(pcap_file):
         # Extract desired fields from each packet
         #print(packet.json())
         try:
             orig_port = packet[IP].sport
         except IndexError:
-            orig_port = '-'
+            orig_port = '0'
 
         try:
             resp_port = packet[IP].dport
         except IndexError:
-            resp_port = '-'
+            resp_port = '0'
 
         try:
-            proto = get_protocol_name(packet[IP].proto)
+            port = packet[IP].proto
+            proto = get_protocol_name(port)
         except IndexError:
-            proto = '-'
-
-        # try:
-        #     service = packet[UDP].dport
-        # except KeyError:
-        service = '-'
+            proto = '0'
 
         try:
-            duration = packet.time
+            try:
+                port = str(packet[IP].proto)
+            except IndexError:
+                service = 'Unkown'
+            service = d[port]
+        except KeyError:
+            service = 'Unkown'
+
+
+        try:
+            duration = float(packet.time)
         except IndexError:
-            duration = '-'
+            duration = '0'
 
         try:
             orig_bytes = packet[IP].len
         except IndexError:
-            orig_bytes = '-'
+            orig_bytes = '0'
 
-        resp_bytes = '-'
+        resp_bytes = '0'
         # try:
         #     resp_bytes = packet[UDP].ack
         # except IndexError and AttributeError:
         #     resp_bytes = '-'
 
-        try:
-            conn_state = packet.sprintf("%flags%") if UDP in packet else None
-        except IndexError:
-            conn_state = '-'
+        # try:
+        #     conn_state = packet.sprintf("%flags%") if UDP in packet else '0'
+        # except IndexError:
+        #     conn_state = '0'
+          
+        if IP in packet:
+            tcp_flags = packet[IP].flags
+            # Determine connection state based on TCP flags
+            if tcp_flags & 0x02 and not tcp_flags & 0x12:
+                conn_state = "S0"
+            elif tcp_flags & 0x02 and tcp_flags & 0x10:
+                conn_state = "SF"
+            elif tcp_flags & 0x08 and not tcp_flags & 0x11:
+                conn_state = "OTH"
+            elif tcp_flags & 0x04:
+                conn_state = "D"
+            else:
+                conn_state = "OTH"
+        else:
+            # If TCP layer is not present, add a placeholder value
+            conn_state = "OTH"
 
-        try:
-            history = packet.sprintf("%TCP.flags%") if UDP in packet else None
-        except IndexError:
-            history = '-'
+        
+        # if UDP in packet:
+        #     # Extract the history field from the UDP payload or relevant header
+        #     # You need to implement this based on the structure of your packet data
+        #     history = extract_history_field(packet)
+
+        #     # Process the history field value
+        # else:
+        history = "NA"
 
         # try:
         #     orig_pkts = packet[UDP].seq
         # except KeyError:
-        orig_pkts = '-'
+        orig_pkts = '0'
 
         # try:
         #     orig_ip_bytes = packet[UDP].window
         # except KeyError:
-        orig_ip_bytes = '-'
+        orig_ip_bytes = '0'
 
         # try:
         #     resp_pkts = packet[UDP].options
         # except KeyError:
-        resp_pkts = '-'
+        resp_pkts = '0'
 
         # try:
         #     resp_ip_bytes = packet[UDP].urgptr
         # except KeyError:
-        resp_ip_bytes = '-'
+        resp_ip_bytes = '0'
 
 
         # Append the extracted information to the list
-        # extracted_info.append([orig_port, resp_port, proto, duration, orig_bytes, 
+        # extracted_info.append([orig_port, resp_port, proto, duration, orig_bytes,
         #                        conn_state, history])
         extracted_info.append([orig_port, resp_port, proto, service, duration, orig_bytes, resp_bytes,
-                        conn_state, history, orig_pkts, orig_ip_bytes, resp_pkts, resp_ip_bytes, '-'])
+                        conn_state, history, orig_pkts, orig_ip_bytes, resp_pkts, resp_ip_bytes, '0'])
 
     return extracted_info
 
 # Example usage
-pcap_file = "packet_pool/test.pcap"
-process_pcap(pcap_file)
+pcap_file = "traffic_analysis/test.pcap"
+perstest = process_pcap(pcap_file)
+
+
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+# columns_to_onehot = [0, 1, 2, 3, 7, 8]
+columns_to_onehot = [2, 3, 7, 8]
+
+data = np.array([['56305', '23', 'tcp', 'Unkown', '0.0', '0', '0', 'S0', 'S', '1', '60', '0', '0', '1'], ['41101', '23', 'tcp', 'Unkown', '0.0', '0', '0', 'S0', 'S', '1', '60', '0', '0', '1']])
+print('Before one-hot encoding features:')
+print(data[1:10])
+print(data[1].shape)
+
+onehot_encoder = OneHotEncoder(sparse_output=True)
+
+dataCopy = data.copy()
+
+addedCols = 0
+for column in columns_to_onehot:
+    column_values = data[:, column]
+    onehot_encoded = onehot_encoder.fit_transform(column_values.reshape(-1, 1)).toarray()
+    dataCopy = np.delete(dataCopy, column + addedCols, axis=1)
+
+    # Insert the new columns
+    for i, encoded_column in enumerate(onehot_encoded.T):
+        dataCopy = np.insert(dataCopy, column + i + addedCols, encoded_column, axis=1)
+
+    addedCols += onehot_encoded.shape[1] - 1
+
+data = dataCopy
+
+
+print('After one-hot encoding features:')
+print(data[1:10])
+print(data[1].shape)
