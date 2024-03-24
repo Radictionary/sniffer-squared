1dc7e58e8d8d695725dcec0470e8b5e3751ac2c3
siIverfish <ivan.g.zimmerman@gmail.com>
2024-03-23 19:25:58 -0700

    pickled socks git add . updategit add .!

--- pickledsocks/pickledsocks.py
+++ pickledsocks/pickledsocks.py
@@ -1,10 +1,17 @@
 import zmq
 import pickle
 
+from typing import Any, Callable
+
 # Create a ZeroMQ context
 context = zmq.Context()
 
-def make_server(f, port):
+
+def make_server(f: Callable, port: int):
+    """
+        f: a handler function which will receive the data from the port.
+        port: the port.
+    """
     socket = context.socket(zmq.REP)
     socket.bind(f"tcp://*:{port}")
     while True:
@@ -12,7 +19,10 @@ def make_server(f, port):
         result = pickle.dumps(f(data))
         socket.send(result)
 
-def send(data, port):
+def send(data: Any, port: int):
+    """
+        send data to server at port. data can be anything.
+    """
     socket = context.socket(zmq.REQ)
     socket.connect(f"tcp://localhost:{port}")
     socket.send(pickle.dumps(data))
