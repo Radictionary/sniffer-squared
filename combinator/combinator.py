363c75183f91b1e54eb07450a3509fe62c8dee43
siIverfish <ivan.g.zimmerman@gmail.com>
2024-03-24 10:02:18 -0700

    combinator

--- combinator/combinator.py
+++ combinator/combinator.py
@@ -1,6 +1,8 @@
 
 import asyncio
 
+from functools import partial
+
 from pickledsocks import picklesocks
 
 # receive data from ports:
@@ -10,33 +12,38 @@ from pickledsocks import picklesocks
 #     3956: arp spoof
 
 # these lists are unnecessary but function as code comments
-tls_handshake_data = []
-kelly_ai_data = []
-dns_spoof_data = []
-arp_spoof_data = []
+statuses = {
+    "tls_handshake_status": "loading...", 
+    "kelly_ai_status": "loading...", 
+    "dns_spoof_status": "loading...", 
+    "arp_spoof_status": "loading...",
+}
 
-history_lists = [[], [], [], []]
-new_lists = [tls_handshake_data, kelly_ai_data, dns_spoof_data, arp_spoof_data]
 ports = [3953, 3954, 3955, 3956]
+
+def status_server(key, new_status):
+    statuses[key] = new_status
+    return "success"
 
 def all_data(_client_data):
     print("Combinator.all_data handler called, sending data to frontend.")
     # doesn't matter what the client sends,
     # they always get the same response: all of the current data.
-    result = [list_.copy() for list_ in new_lists]
-    for list_ in new_lists:
-        history_lists.extend(list_)
-        list_.clear()
-    return result
+    return statuses
+
+tls_handshake_status_server = partial(status_server, key="tls_handshake_status")
+kelly_ai_status_server      = partial(status_server, key="kelly_ai_status")
+dns_spoof_status_server     = partial(status_server, key="dns_spoof_status")
+arp_spoof_status_server     = partial(status_server, key="arp_spoof_status")
 
 async def main():
     print("Combinator.main() coroutine started.")
     await asyncio.gather(
         picklesocks.make_server(all_data, 3952),
-        *[
-            picklesocks.make_server(list_.append, port)
-            for list_, port in zip(lists, ports)
-        ]
+        tls_handshake_status_server,
+        kelly_ai_status_server,
+        dns_spoof_status_server,
+        arp_spoof_status_server,
     )
 
 asyncio.run(main())
